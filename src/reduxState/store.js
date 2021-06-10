3259dc09d19a47e4569791ab13375fc3b2f146be
zjiaxin <18355272970@163.com>
2021-06-10 22:42:11 +0800

    add store描述

--- src/reduxState/store.js
+++ src/reduxState/store.js
@@ -3,14 +3,20 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { persistReducer, persistStore } from 'redux-persist';
 import * as reducers from './reducers';
 
+/* 添加中间件 */
 const middlewares = [];
 
+/* 将多个reducer作为对象整合到一起 */
 export const combinedReducers = combineReducers({
-    userState: persistReducer({
-        key: 'userState',
-        storage: AsyncStorage,
-        blacklist: [],
-    }, reducers.userStateReducer),
+    /* persistReducer实现数据持久化存储 */
+    userState: persistReducer(
+        {
+            key: 'userState',/* 键 */
+            storage: AsyncStorage,/* 存储方式 */
+            blacklist: [],/* 黑名单 */
+        },
+        reducers.userStateReducer
+    ),
 })
 
 export const store = createStore(
